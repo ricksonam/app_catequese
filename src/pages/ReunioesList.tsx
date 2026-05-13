55315283ac303599df6c48bc6e266162db5a34f0
appgestaocatequese <app.gestaocatequese@gmail.com>
2026-05-12 21:08:33 -0300

    fix: resolve JSX syntax error in meeting profile tags to fix deployment failure

--- src/pages/ReunioesList.tsx
+++ src/pages/ReunioesList.tsx
@@ -967,9 +967,10 @@ export default function ReunioesList() {
                   )}
                 </div>
               </div>
-            )}
-          </DialogContent>
-        </Dialog>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
 
       <Dialog open={presencaOpen} onOpenChange={setPresencaOpen}>
         <DialogContent className="rounded-3xl border-border/30 max-w-sm p-0 overflow-hidden">
